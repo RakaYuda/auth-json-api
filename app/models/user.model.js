c41ac682019df916da1b2f3e478551a14125566b
Raka Yuda <raka.yuda.pradipta@gmail.com>
2021-09-03 02:21:40 +0700

    add model for user

---
+++ app/models/user.model.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+
+const userPath = "./app/data/user-data.json";
+
+const addUser = (data) => {
+    const stringifyData = JSON.stringify(data)
+    fs.writeFileSync(userPath, stringifyData)
+}
+
+const getAllUser = () => {
+    const jsonData = fs.readFileSync(userPath)
+    return JSON.parse(jsonData)
+}
+
+const findOne = (username) => {
+    const existAccounts = getAllUser();
+    let isExist = false;
+    let userData = {};
+
+    Object.keys(existAccounts).forEach((account) => {
+        if (existAccounts[account].username === username) {
+            isExist = true;
+            userData = {
+                ...userData,
+                ...existAccounts[account]
+            }
+        }
+    });
+
+    return {
+        isExist, userData
+    }
+}
+
+const userModel = {
+    addUser,
+    getAllUser,
+    findOne
+};
+
+module.exports = userModel;
